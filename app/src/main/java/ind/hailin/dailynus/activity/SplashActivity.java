0cfb6b167fc55c7f7efd52cd01981da9d305f9f6
Hailinx <895227442@qq.com>
2017-05-12 18:24:58 +0800

    second push project directory

--- app/src/main/java/ind/hailin/dailynus/activity/SplashActivity.java
+++ app/src/main/java/ind/hailin/dailynus/activity/SplashActivity.java
@@ -1,5 +1,6 @@
 package ind.hailin.dailynus.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,5 +12,24 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        //get data from server
+        queryServer();
+        //intent to HomeActivity
+        jumpHomeActivity();
     }
+
+    private void queryServer() {
+        //TODO
+    }
+
+    private void jumpHomeActivity() {
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
+        //TODO bundle info
+        finish();
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+    }
+
+
 }
