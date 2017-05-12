cd933c2aaf7392f09dd18ee282f7afe232645e32
Hailinx <895227442@qq.com>
2017-05-12 13:18:35 +0800

    create project directory

---
+++ app/src/main/java/ind/hailin/dailynus/activity/SplashActivity.java
@@ -0,0 +1,15 @@
+package ind.hailin.dailynus.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ind.hailin.dailynus.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
